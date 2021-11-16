e079772b0681f7b18bb346b3afcc226ca86e86c1
Liniplov <levb_28@hotmail.com>
2021-11-16 00:25:48 -0300

    Ajustes de log.

--- fedora-setup.sh
+++ fedora-setup.sh
@@ -40,6 +40,32 @@ HOME="/home/${USER}"
 export LOGDIR=$HOME
 export DATE=`date +"%Y%m%d"`
 export DATETIME=`date +"%Y%m%d_%H%M%S"`
+
+### verbosity levels
+silent_lvl=0
+crt_lvl=1
+err_lvl=2
+wrn_lvl=3
+ntf_lvl=4
+inf_lvl=5
+dbg_lvl=6
+ 
+## esilent prints output even in silent mode
+function esilent () { verb_lvl=$silent_lvl elog "$@" ;}
+function enotify () { verb_lvl=$ntf_lvl elog "$@" ;}
+function eok ()    { verb_lvl=$ntf_lvl elog "SUCCESS - $@" ;}
+function ewarn ()  { verb_lvl=$wrn_lvl elog "${colylw}WARNING${colrst} - $@" ;}
+function einfo ()  { verb_lvl=$inf_lvl elog "${colwht}INFO${colrst} ---- $@" ;}
+function edebug () { verb_lvl=$dbg_lvl elog "${colgrn}DEBUG${colrst} --- $@" ;}
+function eerror () { verb_lvl=$err_lvl elog "${colred}ERROR${colrst} --- $@" ;}
+function ecrit ()  { verb_lvl=$crt_lvl elog "${colpur}FATAL${colrst} --- $@" ;}
+function edumpvar () { for var in $@ ; do edebug "$var=${!var}" ; done }
+function elog() {
+        if [ $verbosity -ge $verb_lvl ]; then
+                datestring=`date +"%Y-%m-%d %H:%M:%S"`
+                echo -e "$datestring - $@"
+        fi
+}
  
 ScriptName=`basename $0`
 Job=`basename $0 .sh`"_log"
@@ -54,6 +80,7 @@ function Log_Open() {
                 mkfifo -m 700 $Pipe
                 LOGFILE=${LOGDIR}/$JobClass/${Job}_${DATETIME}.log
                 exec 3>&1
+                exec 2>&1
                 tee ${LOGFILE} <$Pipe >&3 &
                 teepid=$!
                 exec 1>$Pipe
@@ -544,6 +571,9 @@ echo ""
 echo ""
 echo "FEDORA-SETUP: Installing and configuring AppImage applications."
 echo ""
+
+# Create folder to store AppImage files
+mkdir $HOME/.bin $HOME/.bin/appimagefiles
 
 # Download and install Obsidian
 sudo -E -u $USER wget -q -O "$HOME/.bin/appimagefiles/obisidian.AppImage" -o "/dev/null" \
